06f839fa0aca4d8d7af6a9f5181768d1b6e1f251
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-03-26 18:33:14 -0700

    result button is working now and showing the result of the quizz

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,23 @@ $(document).ready(function() {
    
     
     if ((movie === "jp") || (movie === "ij") &&  (season === "summer") && (power === "Invisibility") || (power === "Flight") && (song === "ws")) {
+      
       $("#rachel").show();
+      
     } else if (((movie === "ij") || (movie === "gf")) && ((season === "winter") || (season === "fall")) && ((power === "Time Travel") || (power === "Super Strength")) && ((song === "rn") || (song === "bm") || (song === "tm"))) {
+      
       $("#monica").show();
     } else if (((movie === "gf") || (movie === "gn")) && ((season === "spring") || (season === "summer")) && ((power === "Mind Reading") || (power === "Teleportation")) && ((song === "qn") || (song === "jc") || (song === "ws"))) {
+      
       $("#phoebe").show();
     } else if (((movie === "fw") || (movie === "jp")) && ((season === "winter") || (season === "fall")) && ((power === "Flight") || (power === "Super Strength")) && ((song === "rn") || (song === "jc") || (song === "bm"))) {
+      
       $("#ross").show();
     } else if (((movie !== "fw") || (movie !== "jp")) && ((season === "spring") || (season !== "winter")) && ((power === "Super Strength") || (power === "Teleportation")) && ((song === "qn") || (song === "jc") || (song === "ws"))) {
-      $("#chandler").show();
+     
+     $("#chandler").show();
     } else if (((movie === "rt") || (movie === "gn")) && ((season === "spring") || (season === "summer") || (season === "fall")) && ((power !== "Mind Reading") || (power === "Teleportation")) && ((song !== "qn") || (song === "jc") || (song === "ws"))) {
+      
       $("#joey").show();
     } else {
       alert("You are nothing!!!!!!");
